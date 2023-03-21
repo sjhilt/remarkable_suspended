a30ca389b7a2cb0e8b12f1f1ecf6a53492ee8f75
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2023-03-21 12:17:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /home/root/bin
-cp suspended.png.bak.png /home/root/bin/suspended.png.bak
+cp suspended.png.bak /home/root/bin/suspended.png.bak
 cp suspended_png.sh /home/root/bin
 cp djaas /home/root/bin
 cp xkcd-dl /home/root/bin
